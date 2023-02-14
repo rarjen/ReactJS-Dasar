a3b883e620191f475df990ecb1e3477d76405c57
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-14 22:47:07 +0700

    Probs validation for Input Probs

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function Input({ type, value, name, onChange }) {
   return <input type={type} value={value} name={name} onChange={onChange} />;
 }
+
+// memberikan default pada prob
+// Input.defaultProps = {
+//   name: "name",
+// };
+
+// Validasi dan handle error untuk props
+Input.PropTypes = {
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
